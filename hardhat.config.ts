aa733a8a26f948ef2ae733c91deeab5b2bd3c2ec
Michael Kim <michael.kim.200518@gmail.com>
2023-08-05 03:51:04 +0900

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -73,15 +73,7 @@ const config: HardhatUserConfig = {
                         enabled: true,
                         runs: 2000,
                     },
-                    outputSelection: {
-                        "*": {
-                            "*": ["storageLayout"],
-                        },
-                    },
                 },
-            },
-            {
-                version: "0.8.16",
             },
         ],
     },
