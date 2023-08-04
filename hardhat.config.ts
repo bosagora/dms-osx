2c7f1621570e38724b824f540305553a354ddc3c
Michael Kim <michael.kim.200518@gmail.com>
2023-08-04 16:46:01 +0900

    Create project

---
+++ hardhat.config.ts
@@ -0,0 +1,130 @@
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-web3";
+import "@typechain/hardhat";
+import * as dotenv from "dotenv";
+import { utils, Wallet } from "ethers";
+import "hardhat-deploy";
+import "hardhat-gas-reporter";
+// tslint:disable-next-line:no-submodule-imports
+import { HardhatUserConfig, task } from "hardhat/config";
+// tslint:disable-next-line:no-submodule-imports
+import { HardhatNetworkAccountUserConfig } from "hardhat/types/config";
+import "solidity-coverage";
+import "solidity-docgen";
+
+dotenv.config({ path: "env/.env" });
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners();
+
+    for (const account of accounts) {
+        console.log(account.address);
+    }
+});
+
+function getAccounts() {
+    const accounts: string[] = [];
+    const reg_bytes64: RegExp = /^(0x)[0-9a-f]{64}$/i;
+    if (
+        process.env.DEPLOYER !== undefined &&
+        process.env.DEPLOYER.trim() !== "" &&
+        reg_bytes64.test(process.env.DEPLOYER)
+    ) {
+        accounts.push(process.env.DEPLOYER);
+    }
+    if (process.env.OWNER !== undefined && process.env.OWNER.trim() !== "" && reg_bytes64.test(process.env.OWNER)) {
+        accounts.push(process.env.OWNER);
+    }
+
+    return accounts;
+}
+
+function getTestAccounts() {
+    const accounts: HardhatNetworkAccountUserConfig[] = [];
+    const defaultBalance = utils.parseEther("2000000").toString();
+
+    const n = 12;
+    for (let i = 0; i < n; ++i) {
+        accounts.push({
+            privateKey: Wallet.createRandom().privateKey,
+            balance: defaultBalance,
+        });
+    }
+    const acc = getAccounts();
+    for (let idx = 0; idx < acc.length; idx++) accounts[idx].privateKey = acc[idx];
+    accounts[0].balance = utils.parseEther("100000000").toString();
+
+    return accounts;
+}
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const config: HardhatUserConfig = {
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.0",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 2000,
+                    },
+                    outputSelection: {
+                        "*": {
+                            "*": ["storageLayout"],
+                        },
+                    },
+                },
+            },
+            {
+                version: "0.8.16",
+            },
+        ],
+    },
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            accounts: getTestAccounts(),
+            gas: 8000000,
+            gasPrice: 8000000000,
+            blockGasLimit: 8000000,
+            deploy: ["./deploy"],
+        },
+        mainnet: {
+            url: process.env.MAIN_NET_URL || "",
+            chainId: 2151,
+            accounts: getAccounts(),
+            deploy: ["./deploy"],
+        },
+        testnet: {
+            url: process.env.TEST_NET_URL || "",
+            chainId: 2019,
+            accounts: getAccounts(),
+            deploy: ["./deploy"],
+        },
+        localhost: {
+            url: "http://localhost:8545",
+            chainId: 1337,
+            accounts: getAccounts(),
+            deploy: ["./deploy"],
+        },
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        owner: {
+            default: 1,
+        },
+    },
+    gasReporter: {
+        enabled: process.env.REPORT_GAS !== undefined,
+        currency: "USD",
+    },
+};
+
+export default config;
