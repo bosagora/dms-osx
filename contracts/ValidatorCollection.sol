a478520ef40105d9fc44b207ba5a5be70fb9b0c2
Michael Kim <michael.kim.200518@gmail.com>
2023-08-05 08:49:43 +0900

    Implement deposit function

--- contracts/ValidatorCollection.sol
+++ contracts/ValidatorCollection.sol
@@ -2,8 +2,14 @@
 
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 /// @notice 검증자들을 저장하는 컨트랙트
 contract ValidatorCollection {
+    uint256 public constant MINIMUM_DEPOSIT_AMOUNT = 50000000000000000000000;
+
+    address private tokenAddress;
+
     /// @notice 검증자의 상태코드
     enum Status {
         INVALID, //  초기값
@@ -20,24 +26,46 @@ contract ValidatorCollection {
     }
 
     /// @notice 검증자 데이타가 저장되는 배열
-    ValidatorData[] public items;
+    address[] public items;
+
+    mapping(address => ValidatorData) public validators;
 
     /// @notice 검증자가 추가될 때 발생되는 이벤트
     event Added(address validator, uint256 start, uint256 balance, Status status);
 
     /// @notice 생성자
-    /// @param validators 초기에 설정될 검증자이다. 예치금이 예치된 후 그 즉시 활성화 된다.
-    constructor(address[] memory validators) {
-        for (uint256 i = 0; i < validators.length; ++i) {
+    /// @param _validators 초기에 설정될 검증자이다. 예치금이 예치된 후 그 즉시 활성화 된다.
+    constructor(address _tokenAddress, address[] memory _validators) {
+        tokenAddress = _tokenAddress;
+
+        for (uint256 i = 0; i < _validators.length; ++i) {
             ValidatorData memory item = ValidatorData({
-                validator: validators[i],
+                validator: _validators[i],
                 start: block.timestamp,
                 balance: 0,
                 status: Status.STOP
             });
-            items.push(item);
+            items.push(_validators[i]);
+            validators[_validators[i]] = item;
 
             emit Added(item.validator, item.start, item.balance, item.status);
         }
+    }
+
+    /// @notice 예치금이 추가로 입급합니다.
+    /// @param _amount 추가로 입금할 예치금
+    function deposit(uint256 _amount) public {
+        ValidatorData memory item = validators[msg.sender];
+        require(item.validator == msg.sender, "Not validator");
+        require(item.status != Status.INVALID, "Not validator");
+        require(item.status != Status.EXIT, "Already exited");
+
+        IERC20 token = IERC20(tokenAddress);
+        require(_amount <= token.allowance(msg.sender, address(this)), "Not allowed deposit");
+        token.transferFrom(msg.sender, address(this), _amount);
+
+        validators[msg.sender].balance += _amount;
+
+        if (validators[msg.sender].balance >= MINIMUM_DEPOSIT_AMOUNT) validators[msg.sender].status = Status.ACTIVE;
     }
 }
--- test/ValidatorCollection.ts
+++ test/ValidatorCollection.ts
@@ -1,4 +1,5 @@
-import { ValidatorCollection } from "../typechain-types";
+import { Token, ValidatorCollection } from "../typechain-types";
+import { Amount } from "./helper/Amount";
 
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-waffle";
@@ -17,25 +18,71 @@ describe("Test for LinkCollection", () => {
 
     const validators = [validator1, validator2, validator3];
     let contract: ValidatorCollection;
+    let tokenContract: Token;
+
+    const amount = Amount.make(50000, 18);
+    const halfAmount = Amount.make(25000, 18);
 
     before(async () => {
+        const tokenFactory = await hre.ethers.getContractFactory("Token");
+        tokenContract = (await tokenFactory.connect(deployer).deploy("Sample", "SAM")) as Token;
+        await tokenContract.deployed();
+        await tokenContract.deployTransaction.wait();
+        for (const elem of validators) {
+            await tokenContract.connect(deployer).transfer(elem.address, amount.value);
+        }
+
         const factory = await hre.ethers.getContractFactory("ValidatorCollection");
-        contract = (await factory.connect(deployer).deploy(validators.map((m) => m.address))) as ValidatorCollection;
+        contract = (await factory.connect(deployer).deploy(
+            tokenContract.address,
+            validators.map((m) => m.address)
+        )) as ValidatorCollection;
         await contract.deployed();
         await contract.deployTransaction.wait();
     });
 
     it("Check validator", async () => {
-        let item = await contract.items(0);
+        let item = await contract.validators(validator1.address);
         assert.deepStrictEqual(item.validator, validator1.address);
         assert.deepStrictEqual(item.status, 2);
 
-        item = await contract.items(1);
+        item = await contract.validators(validator2.address);
         assert.deepStrictEqual(item.validator, validator2.address);
         assert.deepStrictEqual(item.status, 2);
 
-        item = await contract.items(2);
+        item = await contract.validators(validator3.address);
         assert.deepStrictEqual(item.validator, validator3.address);
         assert.deepStrictEqual(item.status, 2);
+    });
+
+    it("Deposit not validator", async () => {
+        await tokenContract.connect(deployer).approve(contract.address, amount.value);
+        await expect(contract.connect(deployer).deposit(amount.value)).to.be.revertedWith("Not validator");
+    });
+
+    it("Deposit not allowed", async () => {
+        await expect(contract.connect(validators[0]).deposit(amount.value)).to.be.revertedWith("Not allowed deposit");
+    });
+
+    it("Deposit 25,000", async () => {
+        for (const elem of validators) {
+            await tokenContract.connect(elem).approve(contract.address, halfAmount.value);
+            await contract.connect(elem).deposit(halfAmount.value);
+            let item = await contract.validators(elem.address);
+            assert.deepStrictEqual(item.validator, elem.address);
+            assert.deepStrictEqual(item.status, 2);
+            assert.deepStrictEqual(item.balance, halfAmount.value);
+        }
+    });
+
+    it("Deposit 50,000", async () => {
+        for (const elem of validators) {
+            await tokenContract.connect(elem).approve(contract.address, halfAmount.value);
+            await contract.connect(elem).deposit(halfAmount.value);
+            let item = await contract.validators(elem.address);
+            assert.deepStrictEqual(item.validator, elem.address);
+            assert.deepStrictEqual(item.status, 1);
+            assert.deepStrictEqual(item.balance, amount.value);
+        }
     });
 });
