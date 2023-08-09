b1f7feefdc1c4bfb1daa0cf0dc6d58e3d0d685f5
Michael Kim <michael.kim.200518@gmail.com>
2023-08-09 12:16:53 +0900

    Add the ability to create a library

---
+++ npm/index.ts
@@ -0,0 +1,6 @@
+// @ts-ignore
+import activeContracts from "../active_contracts.json";
+import artifacts from "./artifacts";
+
+export default artifacts;
+export const activeContractsList = activeContracts;
