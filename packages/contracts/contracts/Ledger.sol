3ea1ab7a729efb3dd1b70b411e39a1682fa002eb
Michael Kim <michael.kim.200518@gmail.com>
2023-08-09 21:15:53 +0900

    Rename npm module 'link-email-wallet'

--- packages/contracts/contracts/Ledger.sol
+++ packages/contracts/contracts/Ledger.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "link-email-wallet/contracts/LinkCollection.sol";
+import "link-email-wallet-osx-artifacts/contracts/LinkCollection.sol";
 import "./ValidatorCollection.sol";
 import "./TokenPrice.sol";
 import "./FranchiseeCollection.sol";
