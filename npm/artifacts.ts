8994497f43ef4718c01dd07a4a9a2c3146236123
Michael Kim <michael.kim.200518@gmail.com>
2023-08-09 12:16:53 +0900

    Add the ability to create a library

---
+++ npm/artifacts.ts
@@ -0,0 +1,17 @@
+// JSON artifacts of the contracts
+
+import * as LinkCollection from "../artifacts/link-email-wallet/contracts/LinkCollection.sol/LinkCollection.json";
+import * as FranchiseeCollection from "../artifacts/contracts/FranchiseeCollection.sol/FranchiseeCollection.json";
+import * as Ledger from "../artifacts/contracts/Ledger.sol/Ledger.json";
+import * as Token from "../artifacts/contracts/Token.sol/Token.json";
+import * as TokenPrice from "../artifacts/contracts/TokenPrice.sol/TokenPrice.json";
+import * as ValidatorCollection from "../artifacts/contracts/ValidatorCollection.sol/ValidatorCollection.json";
+
+export default {
+    LinkCollection,
+    FranchiseeCollection,
+    Ledger,
+    Token,
+    TokenPrice,
+    ValidatorCollection,
+};
