2d30786b4a1a5de28c5080b382d857de14164b13
Michael Kim <michael.kim.200518@gmail.com>
2023-08-09 21:15:53 +0900

    Rename npm module 'link-email-wallet'

--- packages/contracts/npm/artifacts.ts
+++ packages/contracts/npm/artifacts.ts
@@ -1,6 +1,6 @@
 // JSON artifacts of the contracts
 
-import * as LinkCollection from "../artifacts/link-email-wallet/contracts/LinkCollection.sol/LinkCollection.json";
+import * as LinkCollection from "../artifacts/link-email-wallet-osx-artifacts/contracts/LinkCollection.sol/LinkCollection.json";
 import * as FranchiseeCollection from "../artifacts/contracts/FranchiseeCollection.sol/FranchiseeCollection.json";
 import * as Ledger from "../artifacts/contracts/Ledger.sol/Ledger.json";
 import * as Token from "../artifacts/contracts/Token.sol/Token.json";
