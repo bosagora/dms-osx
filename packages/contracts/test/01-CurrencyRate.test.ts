6b85d007eda88d0ce3daf0a010df8ea9d840fac7
Michael Kim <michael.kim.200518@gmail.com>
2023-11-30 20:16:14 +0900

    [Contract] Upgrade CurrencyRate

--- packages/contracts/test/01-CurrencyRate.test.ts
+++ packages/contracts/test/01-CurrencyRate.test.ts
@@ -10,6 +10,8 @@ import { solidity } from "ethereum-waffle";
 
 import * as hre from "hardhat";
 
+import { BigNumber } from "ethers";
+
 chai.use(solidity);
 
 describe("Test for CurrencyRate", () => {
@@ -54,37 +56,84 @@ describe("Test for CurrencyRate", () => {
         const currencyRateFactory = await hre.ethers.getContractFactory("CurrencyRate");
         currencyRateContract = (await currencyRateFactory
             .connect(deployer)
-            .deploy(validatorContract.address)) as CurrencyRate;
+            .deploy(tokenContract.address, validatorContract.address)) as CurrencyRate;
         await currencyRateContract.deployed();
         await currencyRateContract.deployTransaction.wait();
     });
 
-    context("Set", () => {
-        it("Not validator", async () => {
-            const currency = "the9";
-            const price = 123000000000;
-            await expect(currencyRateContract.connect(user1).set(currency, price)).to.revertedWith("1000");
-        });
-        it("Success", async () => {
-            const currency = "the9";
-            const price = 123000000000;
-            await expect(currencyRateContract.connect(validators[0]).set(currency, price))
-                .to.emit(currencyRateContract, "SetPrice")
-                .withNamedArgs({ currency, price });
-        });
+    it("Set Not validator", async () => {
+        const currency = "the9";
+        const rate = 123000000000;
+        await expect(currencyRateContract.connect(user1).set(currency, rate)).to.revertedWith("1000");
+    });
+    it("Set Success", async () => {
+        const currency = "the9";
+        const rate = 123000000000;
+        await expect(currencyRateContract.connect(validators[0]).set(currency, rate))
+            .to.emit(currencyRateContract, "SetRate")
+            .withNamedArgs({ currency, rate });
     });
 
-    context("Get", () => {
-        it("Success - usd", async () => {
-            const currency = "usd";
-            const price = 0;
-            expect(await currencyRateContract.connect(validators[0]).get(currency)).to.equal(price);
-        });
+    it("Get Fail - usd", async () => {
+        const currency = "usd";
+        await expect(currencyRateContract.connect(validators[0]).get(currency)).to.be.revertedWith("1211");
+    });
 
-        it("Success - the9", async () => {
-            const currency = "the9";
-            const price = 123000000000;
-            expect(await currencyRateContract.connect(validators[0]).get(currency)).to.equal(price);
-        });
+    it("Get Success - the9", async () => {
+        const currency = "the9";
+        const rate = 123000000000;
+        expect(await currencyRateContract.connect(validators[0]).get(currency)).to.equal(rate);
+    });
+
+    it("Function", async () => {
+        const multiple = await currencyRateContract.MULTIPLE();
+        const symbol = await tokenContract.symbol();
+
+        let rate = BigNumber.from(100).mul(multiple);
+        await expect(currencyRateContract.connect(validators[0]).set(symbol, rate))
+            .to.emit(currencyRateContract, "SetRate")
+            .withNamedArgs({ rate });
+        rate = BigNumber.from(1000).mul(multiple);
+        await expect(currencyRateContract.connect(validators[0]).set("usd", rate))
+            .to.emit(currencyRateContract, "SetRate")
+            .withNamedArgs({ rate });
+
+        rate = BigNumber.from(10).mul(multiple);
+        await expect(currencyRateContract.connect(validators[0]).set("jpy", rate))
+            .to.emit(currencyRateContract, "SetRate")
+            .withNamedArgs({ rate });
+
+        assert.deepStrictEqual(
+            (await currencyRateContract.convertPointToToken(Amount.make(100).value)).toString(),
+            Amount.make(1).toString()
+        );
+        assert.deepStrictEqual(
+            (await currencyRateContract.convertTokenToPoint(Amount.make(1).value)).toString(),
+            Amount.make(100).toString()
+        );
+        assert.deepStrictEqual(
+            (await currencyRateContract.convertCurrencyToToken(Amount.make(1).value, "usd")).toString(),
+            Amount.make(10).toString()
+        );
+        assert.deepStrictEqual(
+            (await currencyRateContract.convertCurrencyToToken(Amount.make(1).value, "jpy")).toString(),
+            Amount.make(0.1).toString()
+        );
+        assert.deepStrictEqual(
+            (await currencyRateContract.convertCurrencyToPoint(Amount.make(1).value, "usd")).toString(),
+            Amount.make(1000).toString()
+        );
+        assert.deepStrictEqual(
+            (await currencyRateContract.convertCurrencyToPoint(Amount.make(1).value, "jpy")).toString(),
+            Amount.make(10).toString()
+        );
+        assert.deepStrictEqual(
+            (await currencyRateContract.convertCurrency(Amount.make(1).value, "jpy", "usd")).toString(),
+            Amount.make(0.01).toString()
+        );
+        assert.deepStrictEqual(
+            (await currencyRateContract.convertCurrency(Amount.make(1).value, "usd", "jpy")).toString(),
+            Amount.make(100).toString()
+        );
     });
 });
