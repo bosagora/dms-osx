9785fe8226f89a288fa07c5ed29328e1ea01a5f9
Michael Kim <michael.kim.200518@gmail.com>
2023-12-20 17:03:45 +0900

    [Relay] Remove unnecessary code

--- packages/relay/test/Config.test.ts
+++ packages/relay/test/Config.test.ts
@@ -9,7 +9,6 @@ describe("Test of Config", () => {
         config.readFromFile(path.resolve("test", "config.test.yaml"));
         assert.strictEqual(config.server.address, "127.0.0.1");
         assert.strictEqual(config.server.port.toString(), "3000");
-        assert.strictEqual(config.logging.folder, path.resolve("logs"));
         assert.strictEqual(config.logging.level, "debug");
         assert.deepStrictEqual(config.relay.managerKeys, [
             "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
