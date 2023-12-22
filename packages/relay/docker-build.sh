4384544c37c92abe96ca567ba266304d01e91fb0
Michael Kim <michael.kim.200518@gmail.com>
2023-12-22 14:29:03 +0900

    Change docker builder scripts

--- packages/relay/docker-build.sh
+++ packages/relay/docker-build.sh
@@ -3,8 +3,8 @@
 TAG_NANE="$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short=6 HEAD)"
 echo "TAG_NANE=$TAG_NANE"
 
-# docker buildx build --platform=linux/amd64,linux/arm64 -t bosagora/dms-osx-relay:"$TAG_NANE" -f Dockerfile --push .
-# docker buildx build --platform=linux/amd64,linux/arm64 -t bosagora/dms-osx-relay:latest -f Dockerfile --push .
+docker buildx build --platform=linux/amd64,linux/arm64 -t bosagora/dms-osx-relay:"$TAG_NANE" -f Dockerfile --push .
+docker buildx build --platform=linux/amd64,linux/arm64 -t bosagora/dms-osx-relay:latest -f Dockerfile --push .
 
-docker build -t bosagora/dms-osx-relay:"$TAG_NANE" -f Dockerfile  .
-docker push bosagora/dms-osx-relay:"$TAG_NANE"
+#docker build -t bosagora/dms-osx-relay:"$TAG_NANE" -f Dockerfile  .
+#docker push bosagora/dms-osx-relay:"$TAG_NANE"
