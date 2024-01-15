2144bfda125551b1e8cfbb3946d5a855700bd1d1
Michael Kim <michael.kim.200518@gmail.com>
2024-01-15 10:28:53 +0900

    [Validator] Rename node to validator

--- packages/validator/docker-build.sh
+++ packages/validator/docker-build.sh
@@ -3,5 +3,5 @@
 TAG_NANE="$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short=6 HEAD)"
 echo "TAG_NANE=$TAG_NANE"
 
-docker build -t bosagora/dms-node:"$TAG_NANE" -f Dockerfile  .
-docker push bosagora/dms-node:"$TAG_NANE"
+docker build -t bosagora/dms-validator:"$TAG_NANE" -f Dockerfile  .
+docker push bosagora/dms-validator:"$TAG_NANE"
