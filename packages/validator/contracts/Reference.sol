c03edc55d0685e2b77756f2aa7f7bd5512cd9bfd
Michael Kim <michael.kim.200518@gmail.com>
2024-01-27 06:16:16 +0900

    Update Validator

--- packages/validator/contracts/Reference.sol
+++ packages/validator/contracts/Reference.sol
@@ -6,6 +6,7 @@ import "dms-osx-artifacts/contracts/controllers/LoyaltyBurner.sol";
 import "dms-osx-artifacts/contracts/controllers/LoyaltyConsumer.sol";
 import "dms-osx-artifacts/contracts/controllers/LoyaltyProvider.sol";
 import "dms-osx-artifacts/contracts/controllers/LoyaltyExchanger.sol";
+import "dms-osx-artifacts/contracts/controllers/LoyaltyTransfer.sol";
 import "dms-osx-artifacts/contracts/currency/CurrencyRate.sol";
 import "del-osx-artifacts/contracts/PhoneLinkCollection.sol";
 import "dms-osx-artifacts/contracts/ledger/Ledger.sol";
