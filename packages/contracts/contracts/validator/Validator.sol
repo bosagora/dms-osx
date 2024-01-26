361dc9e1cac4c5f9449cd1046ddd7d1a6788ed2a
Michael Kim <michael.kim.200518@gmail.com>
2024-01-27 05:46:59 +0900

    Remove the new participation function of the validator

--- packages/contracts/contracts/validator/Validator.sol
+++ packages/contracts/contracts/validator/Validator.sol
@@ -15,12 +15,6 @@ contract Validator is ValidatorStorage, Initializable, OwnableUpgradeable, UUPSU
     event AddedValidator(address validator, uint256 start, uint256 balance, Status status);
     /// @notice 자금이 입급될 때 발생되는 이벤트
     event DepositedForValidator(address validator, uint256 amount, uint256 balance);
-    /// @notice 검증자의 등록이 요청 되었을 때 발생되는 이벤트
-    event RequestedToJoinValidator(address requester);
-    /// @notice 검증자의 강제 탈퇴가 요청 되었을 때 발생되는 이벤트
-    event RequestedToExitValidator(address requester, address validator);
-    /// @notice 검증자의 자발적 탈퇴가 완료되었을 때 발생되는 이벤트
-    event ExitedFromValidator(address validator);
 
     /// @notice 생성자
     /// @param _tokenAddress 토큰컽트랙트의 주소
@@ -65,43 +59,6 @@ contract Validator is ValidatorStorage, Initializable, OwnableUpgradeable, UUPSU
         if (validators[_msgSender()].balance >= MINIMUM_DEPOSIT_AMOUNT) validators[_msgSender()].status = Status.ACTIVE;
 
         emit DepositedForValidator(_msgSender(), _amount, validators[_msgSender()].balance);
-    }
-
-    /// @notice 신규 검증자 등록을 신청합니다.
-    function requestRegistration() external virtual {
-        require(validators[_msgSender()].status == Status.INVALID, "1003");
-
-        require(MINIMUM_DEPOSIT_AMOUNT <= token.allowance(_msgSender(), address(this)), "1020");
-        token.transferFrom(_msgSender(), address(this), MINIMUM_DEPOSIT_AMOUNT);
-
-        ValidatorData memory item = ValidatorData({
-            validator: _msgSender(),
-            start: block.timestamp + 86500 * 7,
-            balance: MINIMUM_DEPOSIT_AMOUNT,
-            status: Status.ACTIVE
-        });
-
-        items.push(_msgSender());
-        validators[_msgSender()] = item;
-
-        emit RequestedToJoinValidator(_msgSender());
-    }
-
-    /// @notice 검증자의 강제탈퇴를 신청합니다.
-    function requestExit(address validator) external virtual {
-        ValidatorData memory item = validators[_msgSender()];
-        require(item.validator == _msgSender(), "1000");
-        require(item.status == Status.ACTIVE && item.start <= block.timestamp, "1001");
-
-        require(validators[validator].status != Status.INVALID, "1001");
-        validators[validator].status = Status.EXIT;
-
-        if (validators[validator].balance > 0) {
-            token.transfer(validator, validators[validator].balance);
-            validators[validator].balance = 0;
-        }
-
-        emit RequestedToExitValidator(_msgSender(), validator);
     }
 
     /// @notice 등록된 검증자를 리턴한다.
@@ -160,23 +117,5 @@ contract Validator is ValidatorStorage, Initializable, OwnableUpgradeable, UUPSU
     /// @param _account 지갑주소
     function validatorOf(address _account) external view override returns (ValidatorData memory) {
         return validators[_account];
-    }
-
-    /// @notice 자발적으로 탈퇴하기 위해 사용되는 함수입니다.
-    function exit() external virtual {
-        ValidatorData memory item = validators[_msgSender()];
-        require(item.validator == _msgSender(), "1000");
-        require(item.status == Status.ACTIVE && item.start <= block.timestamp, "1001");
-
-        require(_lengthOfActiveValidator() > 1, "1010");
-
-        validators[_msgSender()].status = Status.EXIT;
-
-        if (validators[_msgSender()].balance > 0) {
-            token.transfer(_msgSender(), validators[_msgSender()].balance);
-            validators[_msgSender()].balance = 0;
-        }
-
-        emit ExitedFromValidator(_msgSender());
     }
 }
