ece868952c1d436ca6e36ab8466e79888d86ead7
Michael Kim <michael.kim.200518@gmail.com>
2024-02-20 21:55:25 +0900

    [SubGraph] Update SubGraph

--- packages/faker/docker-build.sh
+++ packages/faker/docker-build.sh
@@ -4,4 +4,4 @@ TAG_NANE="$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short=6 HEAD)"
 echo "TAG_NANE=$TAG_NANE"
 
 docker build -t bosagora/dms-faker:"$TAG_NANE" -f Dockerfile .
-docker push bosagora/dms-faker:"$TAG_NANE"
+# docker push bosagora/dms-faker:"$TAG_NANE"
