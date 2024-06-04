6a16dbbf94a5e555d968ba8fa66bed0bcc1b01cb
Michael Kim <michael.kim.200518@gmail.com>
2024-06-04 14:47:54 +0900

    [SubGraph] Improve the refund settlement amount of shops

--- packages/subgraph-sidechain/src/shop.ts
+++ packages/subgraph-sidechain/src/shop.ts
@@ -1,43 +1,32 @@
 import {
     AddedShop as AddedShopEvent,
     ChangedShopStatus as ChangedShopStatusEvent,
-    IncreasedSettledAmount as IncreasedSettledAmountEvent,
     IncreasedProvidedAmount as IncreasedProvidedAmountEvent,
     IncreasedUsedAmount as IncreasedUsedAmountEvent,
     DecreasedUsedAmount as DecreasedUsedAmountEvent,
     UpdatedShop as UpdatedShopEvent,
-    ClosedWithdrawal as ClosedWithdrawalEvent,
-    OpenedWithdrawal as OpenedWithdrawalEvent,
+    Refunded as RefundedEvent,
 } from "../generated/Shop/Shop";
-import { Shop, ShopTradeHistory, ShopWithdraw } from "../generated/schema";
+import { Shop, ShopTradeHistory } from "../generated/schema";
 import { BigInt } from "@graphprotocol/graph-ts";
 import { AmountUnit } from "./utils";
 
 enum PageType {
     NONE = 0,
     PROVIDE_USE = 1,
-    SETTLEMENT = 2,
-    WITHDRAW = 3,
+    REFUND = 2,
 }
 
 enum ShopAction {
     NONE = 0,
     PROVIDED = 1,
     USED = 2,
-    SETTLED = 3,
-    OPEN_WITHDRAWN = 11,
-    CLOSE_WITHDRAWN = 12,
+    REFUNDED = 3,
 }
 
 enum ShopDataAction {
     CREATED,
     UPDATED,
-    REMOVED,
-}
-
-enum ShopWithdrawStatus {
-    CLOSE = 0,
-    OPEN = 1,
 }
 
 export function handleAddedShop(event: AddedShopEvent): void {
@@ -51,8 +40,7 @@ export function handleAddedShop(event: AddedShopEvent): void {
 
     entity.providedAmount = BigInt.fromI32(0);
     entity.usedAmount = BigInt.fromI32(0);
-    entity.settledAmount = BigInt.fromI32(0);
-    entity.withdrawnAmount = BigInt.fromI32(0);
+    entity.refundedAmount = BigInt.fromI32(0);
 
     entity.blockNumber = event.block.number;
     entity.blockTimestamp = event.block.timestamp;
@@ -89,40 +77,6 @@ export function handleChangedShopStatus(event: ChangedShopStatusEvent): void {
     }
 }
 
-export function handleIncreasedSettledAmount(event: IncreasedSettledAmountEvent): void {
-    let entity = new ShopTradeHistory(event.transaction.hash.concatI32(event.logIndex.toI32()));
-    entity.shopId = event.params.shopId;
-    entity.pageType = PageType.SETTLEMENT;
-    entity.action = ShopAction.SETTLED;
-    entity.cancel = false;
-    entity.increase = event.params.increase.div(AmountUnit);
-    entity.settledAmount = event.params.total.div(AmountUnit);
-    entity.currency = event.params.currency;
-    entity.purchaseId = event.params.purchaseId;
-
-    let shopEntity = Shop.load(event.params.shopId);
-    if (shopEntity !== null) {
-        entity.providedAmount = shopEntity.providedAmount;
-        entity.usedAmount = shopEntity.usedAmount;
-        entity.withdrawnAmount = shopEntity.withdrawnAmount;
-        shopEntity.settledAmount = entity.settledAmount;
-        shopEntity.blockNumber = event.block.number;
-        shopEntity.blockTimestamp = event.block.timestamp;
-        shopEntity.transactionHash = event.transaction.hash;
-        shopEntity.save();
-    } else {
-        entity.providedAmount = BigInt.fromI32(0);
-        entity.usedAmount = BigInt.fromI32(0);
-        entity.withdrawnAmount = BigInt.fromI32(0);
-    }
-
-    entity.blockNumber = event.block.number;
-    entity.blockTimestamp = event.block.timestamp;
-    entity.transactionHash = event.transaction.hash;
-
-    entity.save();
-}
-
 export function handleIncreasedProvidedAmount(event: IncreasedProvidedAmountEvent): void {
     let entity = new ShopTradeHistory(event.transaction.hash.concatI32(event.logIndex.toI32()));
 
@@ -138,8 +92,7 @@ export function handleIncreasedProvidedAmount(event: IncreasedProvidedAmountEven
     let shopEntity = Shop.load(event.params.shopId);
     if (shopEntity !== null) {
         entity.usedAmount = shopEntity.usedAmount;
-        entity.settledAmount = shopEntity.settledAmount;
-        entity.withdrawnAmount = shopEntity.withdrawnAmount;
+        entity.refundedAmount = shopEntity.refundedAmount;
         shopEntity.providedAmount = entity.providedAmount;
         shopEntity.blockNumber = event.block.number;
         shopEntity.blockTimestamp = event.block.timestamp;
@@ -147,8 +100,7 @@ export function handleIncreasedProvidedAmount(event: IncreasedProvidedAmountEven
         shopEntity.save();
     } else {
         entity.usedAmount = BigInt.fromI32(0);
-        entity.settledAmount = BigInt.fromI32(0);
-        entity.withdrawnAmount = BigInt.fromI32(0);
+        entity.refundedAmount = BigInt.fromI32(0);
     }
 
     entity.blockNumber = event.block.number;
@@ -173,8 +125,7 @@ export function handleIncreasedUsedAmount(event: IncreasedUsedAmountEvent): void
     let shopEntity = Shop.load(event.params.shopId);
     if (shopEntity !== null) {
         entity.providedAmount = shopEntity.providedAmount;
-        entity.settledAmount = shopEntity.settledAmount;
-        entity.withdrawnAmount = shopEntity.withdrawnAmount;
+        entity.refundedAmount = shopEntity.refundedAmount;
         shopEntity.usedAmount = entity.usedAmount;
         shopEntity.blockNumber = event.block.number;
         shopEntity.blockTimestamp = event.block.timestamp;
@@ -182,8 +133,7 @@ export function handleIncreasedUsedAmount(event: IncreasedUsedAmountEvent): void
         shopEntity.save();
     } else {
         entity.providedAmount = BigInt.fromI32(0);
-        entity.settledAmount = BigInt.fromI32(0);
-        entity.withdrawnAmount = BigInt.fromI32(0);
+        entity.refundedAmount = BigInt.fromI32(0);
     }
 
     entity.blockNumber = event.block.number;
@@ -208,8 +158,7 @@ export function handleDecreasedUsedAmount(event: DecreasedUsedAmountEvent): void
     let shopEntity = Shop.load(event.params.shopId);
     if (shopEntity !== null) {
         entity.providedAmount = shopEntity.providedAmount;
-        entity.settledAmount = shopEntity.settledAmount;
-        entity.withdrawnAmount = shopEntity.withdrawnAmount;
+        entity.refundedAmount = shopEntity.refundedAmount;
         shopEntity.usedAmount = entity.usedAmount;
         shopEntity.blockNumber = event.block.number;
         shopEntity.blockTimestamp = event.block.timestamp;
@@ -217,8 +166,7 @@ export function handleDecreasedUsedAmount(event: DecreasedUsedAmountEvent): void
         shopEntity.save();
     } else {
         entity.providedAmount = BigInt.fromI32(0);
-        entity.settledAmount = BigInt.fromI32(0);
-        entity.withdrawnAmount = BigInt.fromI32(0);
+        entity.refundedAmount = BigInt.fromI32(0);
     }
 
     entity.blockNumber = event.block.number;
@@ -228,30 +176,28 @@ export function handleDecreasedUsedAmount(event: DecreasedUsedAmountEvent): void
     entity.save();
 }
 
-export function handleOpenedWithdrawal(event: OpenedWithdrawalEvent): void {
+export function handleRefunded(event: RefundedEvent): void {
     let entity = new ShopTradeHistory(event.transaction.hash.concatI32(event.logIndex.toI32()));
     entity.shopId = event.params.shopId;
-    entity.pageType = PageType.WITHDRAW;
-    entity.action = ShopAction.OPEN_WITHDRAWN;
+    entity.pageType = PageType.REFUND;
+    entity.action = ShopAction.REFUNDED;
     entity.cancel = false;
-    entity.increase = event.params.amount.div(AmountUnit);
+    entity.increase = event.params.refundAmount.div(AmountUnit);
     entity.currency = event.params.currency;
 
     let shopEntity = Shop.load(event.params.shopId);
     if (shopEntity !== null) {
         entity.providedAmount = shopEntity.providedAmount;
         entity.usedAmount = shopEntity.usedAmount;
-        entity.settledAmount = shopEntity.settledAmount;
-        entity.withdrawnAmount = shopEntity.withdrawnAmount;
+        entity.refundedAmount = event.params.refundedTotal.div(AmountUnit);
         shopEntity.blockNumber = event.block.number;
         shopEntity.blockTimestamp = event.block.timestamp;
         shopEntity.transactionHash = event.transaction.hash;
         shopEntity.save();
     } else {
         entity.providedAmount = BigInt.fromI32(0);
-        entity.settledAmount = BigInt.fromI32(0);
         entity.usedAmount = BigInt.fromI32(0);
-        entity.withdrawnAmount = BigInt.fromI32(0);
+        entity.refundedAmount = event.params.refundedTotal.div(AmountUnit);
     }
 
     entity.blockNumber = event.block.number;
@@ -259,69 +205,4 @@ export function handleOpenedWithdrawal(event: OpenedWithdrawalEvent): void {
     entity.transactionHash = event.transaction.hash;
 
     entity.save();
-
-    let shopWithdrawEntity = ShopWithdraw.load(event.params.shopId);
-    if (shopWithdrawEntity !== null) {
-        shopWithdrawEntity.status = ShopWithdrawStatus.OPEN;
-        shopWithdrawEntity.withdrawId = event.params.withdrawId;
-        shopWithdrawEntity.amount = event.params.amount;
-        shopWithdrawEntity.currency = event.params.currency;
-        shopWithdrawEntity.account = event.params.account;
-        shopWithdrawEntity.blockNumber = event.block.number;
-        shopWithdrawEntity.blockTimestamp = event.block.timestamp;
-        shopWithdrawEntity.transactionHash = event.transaction.hash;
-        shopWithdrawEntity.save();
-    } else {
-        shopWithdrawEntity = new ShopWithdraw(event.params.shopId);
-        shopWithdrawEntity.status = ShopWithdrawStatus.OPEN;
-        shopWithdrawEntity.withdrawId = event.params.withdrawId;
-        shopWithdrawEntity.amount = event.params.amount;
-        shopWithdrawEntity.currency = event.params.currency;
-        shopWithdrawEntity.account = event.params.account;
-        shopWithdrawEntity.blockNumber = event.block.number;
-        shopWithdrawEntity.blockTimestamp = event.block.timestamp;
-        shopWithdrawEntity.transactionHash = event.transaction.hash;
-        shopWithdrawEntity.save();
-    }
-}
-
-export function handleClosedWithdrawal(event: ClosedWithdrawalEvent): void {
-    let entity = new ShopTradeHistory(event.transaction.hash.concatI32(event.logIndex.toI32()));
-    entity.shopId = event.params.shopId;
-    entity.pageType = PageType.WITHDRAW;
-    entity.action = ShopAction.CLOSE_WITHDRAWN;
-    entity.cancel = false;
-    entity.increase = event.params.amount.div(AmountUnit);
-    entity.withdrawnAmount = event.params.total.div(AmountUnit);
-    entity.currency = event.params.currency;
-
-    let shopEntity = Shop.load(event.params.shopId);
-    if (shopEntity !== null) {
-        entity.providedAmount = shopEntity.providedAmount;
-        entity.settledAmount = shopEntity.settledAmount;
-        entity.usedAmount = shopEntity.usedAmount;
-        shopEntity.withdrawnAmount = entity.withdrawnAmount;
-        shopEntity.blockNumber = event.block.number;
-        shopEntity.blockTimestamp = event.block.timestamp;
-        shopEntity.transactionHash = event.transaction.hash;
-        shopEntity.save();
-    } else {
-        entity.providedAmount = BigInt.fromI32(0);
-        entity.settledAmount = BigInt.fromI32(0);
-        entity.usedAmount = BigInt.fromI32(0);
-    }
-
-    entity.blockNumber = event.block.number;
-    entity.blockTimestamp = event.block.timestamp;
-    entity.transactionHash = event.transaction.hash;
-
-    entity.save();
-
-    let shopWithdrawEntity = ShopWithdraw.load(event.params.shopId);
-    if (shopWithdrawEntity !== null) {
-        if (shopWithdrawEntity.withdrawId == event.params.withdrawId) {
-            shopWithdrawEntity.status = ShopWithdrawStatus.CLOSE;
-            shopWithdrawEntity.save();
-        }
-    }
 }
