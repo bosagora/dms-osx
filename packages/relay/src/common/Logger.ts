800982edf7f2922e0206de75883cabb2c69ffaa9
Danial <win303065@gmail.com>
2024-06-27 17:40:01 +0900

    Remove colorize

--- packages/relay/src/common/Logger.ts
+++ packages/relay/src/common/Logger.ts
@@ -13,7 +13,6 @@ export class Logger {
             handleExceptions: true,
             json: false,
             format: combine(
-                colorize({ all: true }),
                 timestamp({
                     format: "YYYY-MM-DD HH:mm:ss",
                 }),
