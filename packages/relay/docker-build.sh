44b6b32dd41744d028e9fa310e5c46982a7eb3c0
Michael Kim <michael.kim.200518@gmail.com>
2025-08-09 17:11:01 +0900

    build(docker): improve image portability and install reliability for relay

--- packages/relay/docker-build.sh
+++ packages/relay/docker-build.sh
@@ -3,4 +3,4 @@
 TAG_NANE="$(git rev-parse --short=12 HEAD)"
 echo "TAG_NANE=$TAG_NANE"
 
-docker build -t bosagora/kios-relay:"$TAG_NANE" -f Dockerfile --push .
+docker build -t bosagora/kios-relay:"$TAG_NANE" -f Dockerfile --push ../../
--- packages/relay/docker-buildx.sh
+++ packages/relay/docker-buildx.sh
@@ -3,5 +3,5 @@
 TAG_NANE="$(git rev-parse --short=12 HEAD)"
 echo "TAG_NANE=$TAG_NANE"
 
-docker buildx build --platform=linux/amd64,linux/arm64 -t bosagora/kios-relay:"$TAG_NANE" -f Dockerfile --push .
-docker buildx build --platform=linux/amd64,linux/arm64 -t bosagora/kios-relay:latest -f Dockerfile --push .
+docker buildx build --platform=linux/amd64,linux/arm64 -t bosagora/kios-relay:"$TAG_NANE" -f Dockerfile --push ../../
+docker buildx build --platform=linux/amd64,linux/arm64 -t bosagora/kios-relay:latest -f Dockerfile --push ../../
